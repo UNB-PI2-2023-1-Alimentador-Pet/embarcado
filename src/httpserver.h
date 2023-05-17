a431f4feeb96e8f989af4a638539d424738671f7
Wagner M Cunha <wagnermc506@gmail.com>
2023-05-17 20:45:12 -0300

    Estrutura adaptada para o plataformio

---
+++ src/httpserver.h
@@ -0,0 +1,6 @@
+#ifndef _HTTPSERVER_H_
+#define _HTTPSERVER_H_
+
+void start_http_server();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "esp_err.h"
+#include "esp_event.h"
+#include "esp_event_base.h"
+#include "esp_wifi_default.h"
+#include "esp_wifi_types.h"
+#include "hal/esp_flash_err.h"
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_wifi.h"
+#include "string.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "esp_mac.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_wifi_types.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "wifi.h"
+
+void app_main() {
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // wifi_init_softap();
+    // wifi_init_softap_and_sta();
+    wifi_init();
+}
